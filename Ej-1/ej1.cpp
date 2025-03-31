e1195d9f38561f7eeb0ec89f495ab7286e404e6a
rodrigorosinc <rrosincaruso@udesa.edu.ar>
2025-03-31 13:36:03 -0300

    creo q termino el uno.

--- Ej-1/ej1.cpp
+++ Ej-1/ej1.cpp
@@ -82,10 +82,11 @@ class Time {
     }
     // Constructor with hour, minute, second and period
     Time(int h, int m, int s, std::string period) {
-        if (!isValidHour(h)) {throw std::invalid_argument("Invalid hour");}
         if (!isValidMinute(m)) {throw std::invalid_argument("Invalid minute");}
         if (!isValidSecond(s)) {throw std::invalid_argument("Invalid second");}
         if (!isValidPeriod(period)) {throw std::invalid_argument("Invalid period");}
+        if (am_pm == "p.m." && h < 12) {throw std::invalid_argument("Invalid hour");}
+        if (h >= 24 || h < 0) {throw std::invalid_argument("Invalid hour");}
         hours = h;
         minutes = m;
         seconds = s;
@@ -106,17 +107,6 @@ class Time {
         std::cout << "Time in 24h format: " << std::setw(2) << std::setfill('0') << h24 << "h, "
                   << std::setw(2) << std::setfill('0') << minutes << "m, "
                   << std::setw(2) << std::setfill('0') << seconds << "s" << std::endl;
-    }
-
-    void changeTime(int h, int m, int s, std::string period) {
-        if (!isValidHour(h)) {throw std::invalid_argument("Invalid hour");}
-        if (!isValidMinute(m)) {throw std::invalid_argument("Invalid minute");}
-        if (!isValidSecond(s)) {throw std::invalid_argument("Invalid second");}
-        if (!isValidPeriod(period)) {throw std::invalid_argument("Invalid period");}
-        hours = h;
-        minutes = m;
-        seconds = s;
-        am_pm = period;
     }
 
     // Getters
@@ -144,6 +134,42 @@ class Time {
     }
 };
 
+
+void changeTime(Time& t) {
+    bool error = true, change;
+    std::cout << std::endl;
+    std::cout << "Do you want to change the time? (yes (1) - no (0)): ";
+    std::cin >> change;
+    if (change) {
+        while (error) {
+            try {
+                error = false;
+                int h, m, s;
+                std::string period;
+                std::cout << "Enter new hour: ";
+                std::cin >> h;
+                std::cout << "Enter new minute: ";
+                std::cin >> m;
+                std::cout << "Enter new second: ";
+                std::cin >> s;
+                std::cout << "Enter new period (a.m./p.m.): ";
+                std::cin >> period;
+                t.setHours(h); t.setMinutes(m); t.setSeconds(s); t.setAmPm(period);
+                std::cout << "Time changed successfully!" << std::endl;
+                std::cout << std::endl;
+            } catch (std::invalid_argument& e) {
+                error = true;
+                std::cout << e.what() << std::endl;
+            }
+        }
+        t.readTime();
+        t.readTime24hFormat();
+    } else {
+        std::cout << "Time not changed." << std::endl;
+    }
+}
+
+
 int main (){
     bool error = true;
     while(error){
@@ -153,17 +179,18 @@ int main (){
             std::string period;
             std::cout << std::endl;
             std::cout << "Which constructor would you use?" << std::endl;
-            std::cout << "0. Default constructor" << std::endl;
-            std::cout << "1. Constructor with hour" << std::endl;
-            std::cout << "2. Constructor with hour and minute" << std::endl;
-            std::cout << "3. Constructor with hour, minute and second" << std::endl;
-            std::cout << "4. Constructor with hour, minute, second and period" << std::endl;
+            std::cout << "0. Default constructor (a.m. default)" << std::endl;
+            std::cout << "1. Constructor with hour (a.m. default)" << std::endl;
+            std::cout << "2. Constructor with hour and minute (a.m. default)" << std::endl;
+            std::cout << "3. Constructor with hour, minute and second (a.m. default)" << std::endl;
+            std::cout << "4. Constructor with hour, minute, second and period (a.m. default)" << std::endl;
             std::cout << "-> ";
             std::cin >> constructor;
             if (constructor == default_constructor) {
                 Time t1;
                 t1.readTime();
                 t1.readTime24hFormat();
+                changeTime(t1);
             } 
             else if (constructor == first_constructor) {
                 std::cout << "Enter hour: ";
@@ -171,33 +198,50 @@ int main (){
                 Time t2(h);
                 t2.readTime();
                 t2.readTime24hFormat();
+                changeTime(t2);
             } 
             else if (constructor == second_constructor) {
-                std::cout << "Enter hour and minute: ";
-                std::cin >> h >> m;
+                std::cout << "Enter hour: ";
+                std::cin >> h;
+                std::cout << "Enter minute: ";
+                std::cin >> m;
                 Time t3(h, m);
                 t3.readTime();
                 t3.readTime24hFormat();
+                changeTime(t3);
             } 
             else if (constructor == third_constructor) {
-                std::cout << "Enter hour, minute and second: ";
-                std::cin >> h >> m >> s;
+                std::cout << "Enter hour: ";
+                std::cin >> h;
+                std::cout << "Enter minute: ";
+                std::cin >> m;
+                std::cout << "Enter second: ";
+                std::cin >> s;
                 Time t4(h, m, s);
                 t4.readTime();
                 t4.readTime24hFormat();
+                changeTime(t4);
             } 
             else if (constructor == fourth_constructor) {
-                std::cout << "Enter hour, minute, second and period: ";
-                std::cin >> h >> m >> s >> period;
+                std::cout << "Enter hour: ";
+                std::cin >> h;
+                std::cout << "Enter minute: ";
+                std::cin >> m;
+                std::cout << "Enter second: ";
+                std::cin >> s;
+                std::cout << "Enter period (a.m./p.m.): ";
+                std::cin >> period;
                 Time t5(h, m, s, period);
                 t5.readTime();
                 t5.readTime24hFormat();
+                changeTime(t5);
             } 
             else {
                 throw std::invalid_argument("Invalid constructor option");
             }
 
         } catch (std::invalid_argument& e) {
+            std::cout << std::endl;
             error = true;
             std::cout << e.what() << std::endl;
             std::cout << "Please enter valid values." << std::endl;
